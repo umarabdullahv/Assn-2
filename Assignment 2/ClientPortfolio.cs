7a201fde7a5b34abf10ad1528d2189c6065924d0
umarabdullahv <umarabdullah@mail.usf.edu>
2018-09-28 17:26:03 -0400

    initial commit

---
+++ Assignment 2/ClientPortfolio.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_2
+{
+  public class ClientPortfolio
+  {
+    public StockList StockList { get; set; }
+
+    public decimal PortfolioValue { get; set; }
+    public string HolderName { get; set; }
+    public string HolderAddress { get; set; }
+    public string ContactNumber { get; set; }
+
+    // default constructor
+    public ClientPortfolio()
+    {
+      StockList = new StockList();
+      HolderName = null;
+      HolderAddress = null;
+      ContactNumber = null;
+    }
+
+    // Construtor for initialization
+    public ClientPortfolio(string holderName, string holderAddress, string contactNumber)
+    {
+      StockList = new StockList();
+      PortfolioValue = 0.0m;
+      HolderName = holderName;
+      HolderAddress = holderAddress;
+      ContactNumber = contactNumber;
+    }
+  }
+}
