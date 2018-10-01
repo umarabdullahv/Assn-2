a0e41312d687984d360a22b7142737f70777f376
umarabdullahv <umarabdullah@mail.usf.edu>
2018-09-30 23:50:40 -0400

    First Commit

---
+++ Assn 2/Assn2_StockList_Students_2.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_2
+{
+    public partial class StockList
+    {
+        //param   (StockList)listToMerge : second list to be merged 
+        //summary      : merge two different list into a single result list
+        //return       : merged list
+        //return type  : StockList
+        public StockList MergeList(StockList listToMerge)
+        {
+            StockList resultList = new StockList();
+            StockNode current = listToMerge.head;
+
+            resultList.head = head;
+            StockNode currentR = head;
+
+            while (current != null)
+            {
+                resultList.AddStock(current.StockHolding);
+                current = current.Next;
+            }
+
+            return resultList;
+        }
+
+        //param        : NA
+        //summary      : finds the stock with most number of holdings
+        //return       : stock with most shares
+        //return type  : Stock
+        public Stock MostShares()
+        {
+            Stock mostShareStock = null;
+            StockNode current = head;
+            mostShareStock = current.StockHolding;
+
+            while (current != null)
+            {
+                if (current.StockHolding.Holdings > mostShareStock.Holdings)
+                {
+                    mostShareStock = current.StockHolding;
+                }
+                current = current.Next;
+            }
+
+            return mostShareStock;
+        }
+
+        //param        : NA
+        //summary      : finds the number of nodes present in the list
+        //return       : length of list
+        //return type  : int
+        public int Length()
+        {
+            int length = 0;
+
+            // write your implementation here
+
+            StockNode current = head;
+
+            while (current != null)
+            {
+                length = length + 1;
+                current = current.Next;
+            }
+
+            return length;
+        }
+    }
+}
---
+++ Assn 2/Assn2_StockList_Students_3.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_2
+{
+    public partial class StockList
+    {
+        //param        : NA
+        //summary      : Calculate the value of each node by multiplying holdings with price, and returns the total value
+        //return       : total value
+        //return type  : decimal
+        public decimal Value()
+        {
+            decimal value = 0.0m;
+            StockNode current = head;
+
+            while (current != null)
+            {
+                value = value + (current.StockHolding.CurrentPrice) * (current.StockHolding.Holdings);
+                current = current.Next;
+            }
+
+            return value;
+        }
+
+        //param  (StockList) listToCompare     : StockList which has to comared for similarity index
+        //summary      : finds the similar number of nodes between two lists
+        //return       : similarty index
+        //return type  : int
+        public int Similarity(StockList listToCompare)
+        {
+            int similarityIndex = 0;
+            StockNode current = this.head;
+            StockNode current2 = listToCompare.head;
+
+            while (current != null)
+            {
+                while (current2 != null)
+                {
+                    if (current.StockHolding.Symbol == current2.StockHolding.Symbol)
+                    {
+                        if (current.Next == null || current2.Next == null)
+                        {
+                            similarityIndex = similarityIndex + 1;
+                        }
+                        else if (current.StockHolding.Symbol == current.Next.StockHolding.Symbol || current2.StockHolding.Symbol == current2.Next.StockHolding.Symbol)
+                        {
+                            similarityIndex = similarityIndex + 0;
+                        }
+                        else
+                        {
+                            similarityIndex = similarityIndex + 1;
+                        }
+                    }
+                    current2 = current2.Next;
+                }
+                current2 = listToCompare.head;
+                current = current.Next;
+            }
+
+            // write your implementation here
+
+            return similarityIndex;
+        }
+
+        //param        : NA
+        //summary      : Print all the nodes present in the list
+        //return       : NA
+        //return type  : NA
+        public void Print()
+        {
+            // write your implementation here
+            StockNode current = head;
+            while (current != null)
+            {
+                Console.WriteLine(current.StockHolding);
+                current = current.Next;
+            }
+        }
+    }
+}
